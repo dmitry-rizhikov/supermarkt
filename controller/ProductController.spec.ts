bba18eb6006d0ad24c545e32be1e6d71e7c8d9cb
Dmitry Rizhikov <Dmitry.Rizhikov@ext.nintendo.eu>
2025-03-31 17:53:28 +0200

    add test

--- controller/ProductController.spec.ts
+++ controller/ProductController.spec.ts
@@ -1,11 +1,26 @@
-import { describe, expect, it, vi } from 'vitest'
-import { ProductControllerImpl } from './ProductController'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { ProductControllerImpl } from '~~/controller/ProductController'
+import { ProductRepositoryImpl } from '~~/repository/ProductRepository'
 
-describe('ProductController', () => {
-  useStorage('supermarket')
+describe('ProductController', async () => {
+  beforeEach(() => {
+    vi.spyOn(
+      ProductRepositoryImpl.prototype,
+      'getProductInfo',
+    ).mockResolvedValue({ price: 30, offerPrice: 45, offerAmount: 2 })
+    vi.spyOn(
+      ProductRepositoryImpl.prototype,
+      'getProductNames',
+    ).mockResolvedValue(['banana', 'apple', 'kiwi'])
+  })
 
-  it.each([['banana', ['banana']]])(
-    'should get the expected product names from scan string',
+  it.each([
+    ['banana', ['banana']],
+    ['banana,apple,kiwi,peach', ['banana', 'apple', 'kiwi', 'peach']],
+    ['banana^apple%kiwi(peach@', ['banana', 'apple', 'kiwi', 'peach']],
+    ['bread,butter', null],
+  ])(
+    'should get the expected product names from scan string "%s"',
     async (scanString, namesArray) => {
       expect(
         await new ProductControllerImpl().getProductNames(scanString),
